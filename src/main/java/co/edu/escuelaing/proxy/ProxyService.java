ac4cea405a85e412f2885088401d0523724e0a54
unknown <cristian.forero-m@labinfo.is.escuelaing.edu.co>
2022-10-26 18:40:37 -0500

    Servicio proxy Terminado

--- src/main/java/co/edu/escuelaing/proxy/ProxyService.java
+++ src/main/java/co/edu/escuelaing/proxy/ProxyService.java
@@ -1,11 +1,12 @@
-
 package co.edu.escuelaing.proxy;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import static spark.Spark.*;
+
 /**
  *
  * @author cristian.forero-m
@@ -15,40 +16,45 @@ public class ProxyService {
     private static final String USER_AGENT = "Mozilla/5.0";
     private static final String GET_URL = "http://127.0.0.1:8080/palindrome";
 
-    
     public static void main(String... args) throws IOException {
         port(getPort());
-        staticFiles.location("/static");
-        get("/service",(req,res)-> "Service is UP");
-        /*URL obj = new URL(GET_URL);
-        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-        con.setRequestMethod("GET");
-        con.setRequestProperty("User-Agent", USER_AGENT);
-        
-        //The following invocation perform the connection implicitly before getting the code
-        int responseCode = con.getResponseCode();
-        System.out.println("GET Response Code :: " + responseCode);
-        
-        if (responseCode == HttpURLConnection.HTTP_OK) { // success
-            BufferedReader in = new BufferedReader(new InputStreamReader(
-                    con.getInputStream()));
-            String inputLine;
-            StringBuffer response = new StringBuffer();
+        staticFiles.location("/public");
+        get("/palindrome", (req, res) -> {
+            String s = "404 NOT FOUND :*";
+            URL obj = new URL(GET_URL+"?value="+req.queryParams("value"));
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+            con.setRequestMethod("GET");
+            con.setRequestProperty("User-Agent", USER_AGENT);
 
-            while ((inputLine = in.readLine()) != null) {
-                response.append(inputLine);
+            //The following invocation perform the connection implicitly before getting the code
+            int responseCode = con.getResponseCode();
+            System.out.println("GET Response Code :: " + responseCode);
+
+            if (responseCode == HttpURLConnection.HTTP_OK) { // success
+                BufferedReader in = new BufferedReader(new InputStreamReader(
+                        con.getInputStream()));
+                String inputLine;
+                StringBuffer response = new StringBuffer();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+
+                // print result
+                System.out.println(response.toString());
+                s = response.toString();
+            } else {
+                System.out.println("GET request not worked");
             }
-            in.close();
-
-            // print result
-            System.out.println(response.toString());
-        } else {
-            System.out.println("GET request not worked");
-        }
-        System.out.println("GET DONE");*/
+            System.out.println("GET DONE");
+            return s;
+        });
+        /**/
     }
-    public static int getPort(){
-        if (System.getenv("PORT") != null){
+
+    public static int getPort() {
+        if (System.getenv("PORT") != null) {
             return Integer.parseInt(System.getenv("PORT"));
         }
         return 8081;
